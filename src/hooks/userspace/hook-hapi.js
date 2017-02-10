eee6387f45fb287196768a81d5bceacfa524b156
Kelvin Jin <kelvinjin@google.com>
2017-02-10 10:03:33 -0800

    Update hook-hapi.js

--- src/hooks/userspace/hook-hapi.js
+++ src/hooks/userspace/hook-hapi.js
@@ -13,61 +13,69 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 'use strict';
 
-var cls = require('../../cls.js');
-var TraceLabels = require('../../trace-labels.js');
 var shimmer = require('shimmer');
-var semver = require('semver');
-var constants = require('../../constants.js');
 var urlParse = require('url').parse;
-var agent;
+
+var constants = require('../constants.js');
+var TraceLabels = require('../trace-labels.js');
 
 var SUPPORTED_VERSIONS = '8 - 16';
 
-function connectionWrap(connection) {
+function connectionWrap(api, connection) {
   return function connectionTrace() {
     var server = connection.apply(this, arguments);
-    server.ext('onRequest', middleware);
+    server.ext('onRequest', middleware.bind(null, api));
     return server;
   };
 }
 
-function middleware(request, reply) {
-  var namespace = cls.getNamespace();
-  if (!namespace) {
-    agent.logger.info('Hapi: no namespace found, ignoring request');
-    return reply.continue();
-  }
+function middleware(api, request, reply) {
   var req = request.raw.req;
   var res = request.raw.res;
-  var traceHeader = agent.parseContextFromHeader(
-    req.headers[constants.TRACE_CONTEXT_HEADER_NAME]) || {};
-  if (!agent.shouldTrace(req.url, traceHeader.options)) {
-    return reply.continue();
-  }
-
-  namespace.bindEmitter(req);
-  namespace.bindEmitter(res);
-
   var originalEnd = res.end;
-
-  namespace.run(function() {
-    var rootContext = startRootSpanForRequest(req, traceHeader);
-    var context = agent.generateTraceContext(rootContext, true);
-    if (context) {
-      res.setHeader(constants.TRACE_CONTEXT_HEADER_NAME, context);
-    } else {
-      agent.logger.warn('hapi: Attempted to generate trace context for nullSpan');
+  var options = {
+    name: urlParse(req.url).pathname,
+    traceContext: req.headers[constants.TRACE_CONTEXT_HEADER_NAME],
+    skipFrames: 3
+  };
+  api.runInRootSpan(options, function(transaction) {
+    if (!transaction) {
+      // TODO: Determine if this message is still needed
+      //console.info('Hapi: no namespace found, ignoring request');
+      return reply.continue();
     }
+
+    api.wrapEmitter(req);
+    api.wrapEmitter(res);
+
+    var url = (req.headers['X-Forwarded-Proto'] || 'http') +
+    '://' + req.headers.host + req.url;
+  
+    // we use the path part of the url as the span name and add the full
+    // url as a label
+    // req.path would be more desirable but is not set at the time our middlewear runs.
+    transaction.addLabel(TraceLabels.HTTP_METHOD_LABEL_KEY, req.method);
+    transaction.addLabel(TraceLabels.HTTP_URL_LABEL_KEY, url);
+    transaction.addLabel(TraceLabels.HTTP_SOURCE_IP, req.connection.remoteAddress);
+
+    var context = transaction.getTraceContext();
+    res.setHeader(constants.TRACE_CONTEXT_HEADER_NAME, context);
 
     // wrap end
     res.end = function(chunk, encoding) {
       res.end = originalEnd;
       var returned = res.end(chunk, encoding);
 
-      endRootSpanForRequest(rootContext, req, res);
+      if (req.route && req.route.path) {
+        transaction.addLabel(
+          'hapi/request.route.path', req.route.path);
+      }
+      transaction.addLabel(
+          TraceLabels.HTTP_RESPONSE_CODE_LABEL_KEY, res.statusCode);
+      transaction.endSpan();
+
       return returned;
     };
 
@@ -75,61 +83,14 @@ function middleware(request, reply) {
   });
 }
 
-/**
- * Creates and sets up a new root span for the given request.
- * @param {Object} req The request being processed.
- * @param {Object} traceHeader The incoming trace header.
- * @returns {!SpanData} The new initialized trace span data instance.
- */
-function startRootSpanForRequest(req, traceHeader) {
-  var traceId = traceHeader.traceId;
-  var parentSpanId = traceHeader.spanId;
-  var url = (req.headers['X-Forwarded-Proto'] || 'http') +
-    '://' + req.headers.host + req.url;
-
-  // we use the path part of the url as the span name and add the full
-  // url as a label
-  // req.path would be more desirable but is not set at the time our middlewear runs.
-  var rootContext = agent.createRootSpanData(urlParse(req.url).pathname, traceId, parentSpanId, 3);
-  rootContext.addLabel(TraceLabels.HTTP_METHOD_LABEL_KEY, req.method);
-  rootContext.addLabel(TraceLabels.HTTP_URL_LABEL_KEY, url);
-  rootContext.addLabel(TraceLabels.HTTP_SOURCE_IP, req.connection.remoteAddress);
-  return rootContext;
-}
-
-
-/**
- * Ends the root span for the given request.
- * @param {!SpanData} rootContext The trace context to close out.
- * @param {Object} req The request being processed.
- * @param {Object} res The response being processed.
- */
-function endRootSpanForRequest(rootContext, req, res) {
-  if (req.route && req.route.path) {
-    rootContext.addLabel(
-      'hapi/request.route.path', req.route.path);
-  }
-  rootContext.addLabel(
-      TraceLabels.HTTP_RESPONSE_CODE_LABEL_KEY, res.statusCode);
-  rootContext.close();
-}
-
-module.exports = function(version_, agent_) {
-  if (!semver.satisfies(version_, SUPPORTED_VERSIONS)) {
-    agent_.logger.info('Hapi: unsupported version ' + version_ + ' loaded');
-    return {};
-  }
-  return {
-    // An empty relative path here matches the root module being loaded.
-    '': {
-      patch: function(hapi) {
-        agent = agent_;
-        shimmer.wrap(hapi.Server.prototype, 'connection', connectionWrap);
-      },
-      unpatch: function(hapi) {
-        shimmer.unwrap(hapi.Server.prototype, 'connection');
-        agent_.logger.info('Hapi: unpatched');
-      }
+module.exports = [
+  {
+    file: '',
+    versions: SUPPORTED_VERSIONS,
+    patch: function(hapi, api) {
+      shimmer.wrap(hapi.Server.prototype,
+                   'connection',
+                   connectionWrap.bind(null, api));
     }
-  };
-};
+  }
+];
