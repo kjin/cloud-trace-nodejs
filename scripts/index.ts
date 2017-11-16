4f1a58586994d8d26711269850f76e587a204fba
Kelvin Jin <kelvinjin@google.com>
2017-11-15 18:46:07 -0800

    refactor: add web framework plugin types and script to fetch plugin types

--- scripts/index.ts
+++ scripts/index.ts
@@ -2,6 +2,7 @@ const [bin, script, ...steps] = process.argv;
 
 import { checkInstall } from './check-install';
 import { encryptCredentials, decryptCredentials } from './credentials';
+import { getPluginTypes } from './get-plugin-types';
 import { initTestFixtures } from './init-test-fixtures';
 import { reportCoverage } from './report-coverage';
 import { runTests } from './run-tests';
@@ -47,6 +48,9 @@ async function run(steps: string[]) {
         }
 
         await decryptCredentials({ key, iv }, `node-team-test-${keyID}.json`);
+        break;
+      case 'get-plugin-types':
+        await getPluginTypes();
         break;
       case 'init-test-fixtures':
         await initTestFixtures();
